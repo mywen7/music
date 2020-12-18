d0cbb981f080109f133463a672bcca08c92e2430
myw15226262705 <2848662558@qq.com>
2020-12-18 18:03:38 +0800

    feat

--- src/layout/header.vue
+++ src/layout/header.vue
@@ -1,11 +1,61 @@
 <template>
   <div class="header"> 
     <div class="left">
-      <div>
-
+      <div class="buttons">
+        <div
+          @click="onClickLogo"
+          class="mac-button red"
+        >
+          <Icon
+            :size="9"
+            type="home"
+          />
+        </div>
+        <div
+          @click="exitFullscreen"
+          class="mac-button yellow"
+        >
+          <Icon
+            :size="9"
+            type="minus"
+          />
+        </div>
+        <div
+          @click="fullscreen"
+          class="mac-button green"
+        >
+          <Icon
+            :size="9"
+            type="fullscreen"
+          />
+        </div>
       </div>
+      <!-- 缩起播放器 -->
+      <div
+        @click="onClickDown"
+        class="shrink-player"
+        v-if="isPlayerShow"
+      >
+        <Icon
+          :backdrop="true"
+          type="down"
+        />
+      </div>
+      <!-- 路由记录器 -->
+      <!-- <div
+        class="history"
+        v-show="!isPlayerShow"
+      >
+        <RoutesHistory />
+      </div>
+
+      <div class="right">
+        <div class="search-wrap">
+          <Search />
+        </div>
+        <Theme />
+      </div> -->
     </div>
-    <div class="right"></div>
   </div>
 </template>
 
@@ -21,3 +71,9 @@ export default defineComponent({
   },
 });
 </script>
+<style lang="scss" scoped>
+  .header {
+    display: flex;
+    justify-content: space-between;
+  }
+</style>
