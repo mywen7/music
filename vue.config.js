6d3027a9a62d158725ecbbd88378a1f912196b0e
myw15226262705 <2848662558@qq.com>
2021-01-09 17:36:56 +0800

    hotfix update

--- vue.config.js
+++ vue.config.js
@@ -4,8 +4,8 @@ module.exports = {
       proxy: {
         '/api': {
           target: 'http://localhost:3000',
-          changeOrigin: true,
-          pathRewrite: { '/api': '' }
+          pathRewrite: {'^/api' : ''},
+          changeOrigin: true
         },
       },
     },
