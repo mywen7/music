de9ced06a73a7e800ae97bc3e2c542a6ffeace03
myw15226262705 <2848662558@qq.com>
2021-02-05 16:36:15 +0800

    feat 新增vuex全局状态管理

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,12 @@ import Elementplus from 'element-plus';
 import 'element-plus/lib/theme-chalk/index.css';
 import App from './App.vue';
 import router from './router/index';
+import store from './store/index';
 import { globalComponent } from './libs/global';
 const app = createApp(App);
 
 app.use(Elementplus);
 app.use(router);
+app.use(store);
 app.use(globalComponent);
 app.mount('#app');
